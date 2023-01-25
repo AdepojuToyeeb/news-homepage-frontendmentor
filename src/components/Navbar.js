60bbf5ecdf753ebf614b7ecee14f295e7c80dedc
Adepoju Adewale <adewale.adepoju14@gmail.com>
2023-01-25 15:07:49 +0100

    working on burger-nav functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import "./Navbar.css";
 import header from "../Images/W..svg";
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(true)
+  const toggleChange = () => {
+    setIsNavOpen(!isNavOpen);
+    console.log(isNavOpen)
+  }
   return (
     <header>
       <img src={header} alt="company-logo" />
-      <nav>
+      <div onClick={toggleChange} className={isNavOpen?"burger":"burger2"}>
+        <hr className="line"/>
+        <hr className="line"/>
+        <hr className="line"/>
+      </div>
+      <div >
+        <h1 onClick={toggleChange} className={isNavOpen?"close":"close2"}>X</h1>
+      </div>
+
+
+      <nav className={isNavOpen?"navigation":"navigation2"}>
         <ul>
           <li>
             <a href="google.com">Home</a>
